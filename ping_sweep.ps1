bfa5917ff8b9dcdf6cd87df7fe755decd648ce40
Ug0Security <c.vallicari@gmail.com>
2022-09-09 09:44:23 +0200

    Create ping_sweep.ps1

---
+++ ping_sweep.ps1
@@ -0,0 +1,23 @@
+For($i=0;$i -lt 254 ;$i++) 
+{
+   $truc = "192.168." + $i.ToString() + ".11"
+   
+   $arg = $truc + " -n 1"
+   
+   $maximumRuntimeSeconds = 1
+ 
+       $process = Start-Process -FilePath C:\Windows\System32\ping.exe -ArgumentList $arg -PassThru -RedirectStandardOutput c:\ziwit\list_ping.txt  -NoNewWindow
+ 
+    try 
+        {
+        $process | Wait-Process -Timeout $maximumRuntimeSeconds -ErrorAction Stop 
+        }
+    catch 
+        {
+        $process | Stop-Process -Force 
+        } 
+        Get-Content c:\ziwit\list_ping.txt
+}
+
+
+
